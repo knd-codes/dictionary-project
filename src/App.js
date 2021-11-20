c13fac14931d8bbdd8f6b908c30d392b801b02d4
Krista <krista.nicole02@gmail.com>
2021-11-20 13:40:04 -0600

    final dictionary

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <img src= {logo} className="App-logo img-fluid"
-        alt="logo" />
+        <img src= {logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <p>
         <Dictionary />
       </p>
       <footer className="App-footer">
-        Coded by Krista Gonzalez
+        <small>
+        This project was coded by <a href="https://github.com/knd-codes/dictionary-project"> Krista Gonzalez </a>
+         and hosted on <a href="https://fervent-colden-c5d26b.netlify.app">Netlify</a>.
+        </small>
       </footer>
     </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,59 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results"
 import "./Dictionary.css";
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
+    let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
-    function handleResponse(response) {
-        console.log(response.data[0]);
-    }
-   function search(event) {
-    event.preventDefault();
+    function handleDictionResponse(response) {
+        setResults(response.data[0]);
+  }
 
+    function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-  }   
+    axios.get(apiUrl).then(handleDictionResponse);
+  }
+
+    function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
     function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+    setKeyword(event.target.value);
+  }
 
+    function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
     return (
-        <div className="Dictionary">
-           <form onSubmit={search}>
-               <input type="search" onChange={handleKeywordChange} />
-           </form>
-        </div>
+      <div className="Dictionary">
+          <center>
+          <h1>
+                  Welcome! <br /><br />
+          What word do you want to search?
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          </center>
+        <section>
+        <Results results={results} />
+        </section>
+      </div>
     );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
