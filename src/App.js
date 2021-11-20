6250746105f6d44d6fc6c4165ddc259039cf1f82
Krista <krista.nicole02@gmail.com>
2021-11-20 10:42:48 -0600

    search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src= {logo} 
-        className="App-logo img-fluid"
+        <img src= {logo} className="App-logo img-fluid"
         alt="logo" />
       </header>
+      <p>
+        <Dictionary />
+      </p>
+      <footer className="App-footer">
+        Coded by Krista Gonzalez
+      </footer>
+    </div>
     </div>
   );
 }
